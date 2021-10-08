40a9f10f4ead3bf5f9c5feb69e6274672033a0c1
Aleandro2000 <carmicialexandru@yahoo.com>
2021-10-08 21:32:33 +0300

    pokemon

--- src/setupProxy.js
+++
@@ -1,5 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function(app) {
-    app.use(createProxyMiddleware('/api/v2/**', { target: 'https://pokeapi.co' }));
-};
